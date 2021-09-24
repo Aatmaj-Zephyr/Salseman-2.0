57f1fbfd3b21e291f2c2faf3e0b3268ca2435b48
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-09-24 17:30:41 +0530

    Update initialize.js

--- initialize.js
+++ initialize.js
@@ -2,3 +2,5 @@ document.getElementById('addcity').innerHTML="Add city";
 document.getElementById('cityname').innerHTML="Name of city";
 document.getElementById('xposlbl').innerHTML="x coordinate of the city";
 document.getElementById('yposlbl').innerHTML="y coordinate of the city";
+document.getElementById('nbrlbl').innerHTML="Please enter the neighbours";
+document.getElementById('submit').innerHTML="Submit";
