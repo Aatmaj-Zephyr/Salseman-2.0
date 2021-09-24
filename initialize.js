30cc27b245e35d0980671059c365e49e41720891
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-09-24 16:07:53 +0530

    Create initialize.js

---
+++ initialize.js
@@ -0,0 +1,4 @@
+document.getElementById('addcity').innerHTML="Add city";
+document.getElementById('cityname').innerHTML="Name of city";
+document.getElementById('xposlbl').innerHTML="x coordinate of the city";
+document.getElementById('yposlbl').innerHTML="y coordinate of the city";
