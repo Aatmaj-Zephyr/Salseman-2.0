9fc97a82b31485356023e10248ffa42a8adb8e68
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-09-24 16:28:37 +0530

    Update Main.js

--- Main.js
+++ Main.js
@@ -2,3 +2,25 @@ function addcity(){
     document.getElementById('form').style.display="inline";
     document.getElementById('addcity').style.display="none";
 }
+function submit(){
+    if(!document.getElementById('citytext').checkValidity()){
+        window.alert("Error: Please fill out the name of the city")
+    }
+    else if(!document.getElementById('xpos').checkValidity()){
+        window.alert("Error: Please fill out the x coordinate of the city")
+    }
+    else if(!document.getElementById('ypos').checkValidity()){
+        window.alert("Error: Please fill out the y coordinate of the city")
+    }
+    else{
+        Add(document.getElementById('ypos').value)
+    }
+}
+function Add(a){
+    //document.getElementById('submit').innerHTML=a
+    /* Put in Data base if not present already
+    ..
+    ...
+    ...
+    */
+}
