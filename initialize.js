3246dc1fd6c115a12fa296b0a95cefaf1eadb353
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-09-25 12:17:49 +0530

    Update initialize.js

--- initialize.js
+++ initialize.js
@@ -5,5 +5,9 @@ document.getElementById('xposlbl').innerHTML="x coordinate of the city";
 document.getElementById('yposlbl').innerHTML="y coordinate of the city";
 document.getElementById('nbrlbl').innerHTML="Please enter the neighbours";
 document.getElementById('submit').innerHTML="Submit";
+document.getElementById('setroute').innerHTML="Find Route";
+document.getElementById('ctylbl').innerHTML="Please select the cities to be visited";
+document.getElementById('submitcty').innerHTML="Submit";
+
 }
-initialize()
+initialize();
